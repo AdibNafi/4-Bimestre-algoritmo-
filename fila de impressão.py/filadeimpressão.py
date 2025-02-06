06354f5450fa9e677d2120bb389ebb9267f1aca1
adib <nafiyas2007@gmail.com>
2025-02-06 14:35:51 -0400

    filadeimpressão.py

--- fila de impressão.py/filadeimpressão.py
+++ fila de impressão.py/filadeimpressão.py
@@ -2,32 +2,64 @@
 #IMPRESSORA QUE PODE RECEBER IMPRESSAO DE DIVERSOS 
 #COMPUTADORES, VAMOS PENSAR EM UMA FILA.
 
-#funçoes de interação com o usuario 
-def menu():
-    fila_impressão=filaDeImpressao()
-    #criando uma instancia para a classe
-    fila_impressão.configurar_inicial()
-    #configurar os atributos de entrada/inicial 
-    while true:
-        #opção para o nosso usuario
-        print("Opções:")
-        print("1 Adiciona um trabalho na fila da impressao")
-        print("2. imprimir o proximo trabalho da fila")
-        print("3. mostrar a fila de impressao")
-        print("4. sair")
-        escolha = input("escolha uma opcao 1-4")
-         #aqui vai ser lido a escolha do usuario
+class FilaDeImpressao:
 
-    if escolha== "1":
-        trabalho = input("digite o nome do trabalho a ser impresso")
-        #maquina da pessoa que esta imprimindo 
-        fila_impressao.adicionar_trabalho(trabalho)
-    elif escolha=="2":
-        fila_impressão.processar_trabalho()
-    elif escolha=="3":
-        fila_impressão.mostrar_fila()
-    elif escolha=="4":
-        print("saindo do programa")
-        break
-    else:
-        print("opcao invalida")
+
+    def configurar_inicial(self):
+       self.fila= []
+        #isso vai guardar a fila de impressao no vetor fila
+
+    def adicionar_trabalho(self,trabalho):
+        self.fila.append(trabalho)
+        print(f"Trabalho '{trabalho}' adicionado a fila de impressão")
+
+#remover o ytabalho da lista 
+    def processar_trabalho(self):
+        if not self.esta_vazia():
+            trabalho = self.fila.pop(0)
+            print(f"o trabalho '{trabalho}' esta sendo processado")
+        else:
+            print("A fila esta vazia!")
+    
+    def mostrar_fila(self):
+        if self.esta_vazia():
+            print("A fila está vazia!")
+        else:
+            print("Fila atual de impressão:") 
+            for trabalho in self.fila:
+                print(f"-{trabalho}")    
+    def esta_vazia(self):
+        return len(self.fila) == 0
+
+
+    #funçoes de interação com o usuario 
+def menu():
+        fila_impressao= FilaDeImpressao()
+        #criando uma instancia para a classe
+        fila_impressao.configurar_inicial()
+        #configurar os atributos de entrada/inicial 
+        while True:
+            #opção para o nosso usuario
+            print("/n Opções:")
+            print("1 Adiciona um trabalho na fila da impressao")
+            print("2. imprimir o proximo trabalho da fila")
+            print("3. mostrar a fila de impressao")
+            print("4. sair")
+            escolha = input("escolha uma opcao 1-4")
+            #aqui vai ser lido a escolha do usuario
+
+            if escolha == "1":
+                trabalho = input("digite o nome do trabalho a ser impresso")
+                #maquina da pessoa que esta imprimindo 
+                fila_impressao.adicionar_trabalho(trabalho)
+            elif escolha=="2":
+                    fila_impressao.processar_trabalho()
+            elif escolha=="3":
+                    fila_impressao.mostrar_fila()
+            elif escolha=="4":
+                print("saindo do programa")
+                break
+            else:
+                    print("opcao invalida")
+
+menu()
